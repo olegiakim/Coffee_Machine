84fee8bade8843516ab5168c151e12c948015ccf
Oleg Iakim <execute067@gmail.com>
2021-08-14 00:35:22 -0700

    2 task done

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write how many ml of water the coffee machine has:");
@@ -13,13 +12,35 @@ public class CoffeeMachine {
         System.out.println("Write how many ml of milk the coffee machine has:");
         int milk = scanner.nextInt();
         System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee = scanner.nextInt();
+        int beans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need: ");
         int cupsOfCoffee = scanner.nextInt();
 
+        if(cupsOfCoffee == 0){
+            System.out.println("Yes, I can make that amount of coffee");
+            System.exit(0);
+        }
+
+        int portionOfWater = water / 200;
+        int portionOfMilk = milk / 50;
+        int portionOfBeans = beans / 15;
 
 
+        int totalCupsOfCoffee = 0;
+        if(portionOfWater < portionOfMilk && portionOfWater < portionOfBeans){
+            totalCupsOfCoffee = portionOfWater;
+        } else if (portionOfMilk < portionOfBeans) {
+            totalCupsOfCoffee = portionOfMilk;
+        } else {
+            totalCupsOfCoffee = portionOfBeans;
+        }
 
-
+        if(cupsOfCoffee == totalCupsOfCoffee){
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsOfCoffee <= totalCupsOfCoffee){
+            System.out.println("Yes, I can make that amount of coffee (and even " + (totalCupsOfCoffee - cupsOfCoffee ) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + (totalCupsOfCoffee) +" cup(s) of coffee");
+            }
     }
 }
